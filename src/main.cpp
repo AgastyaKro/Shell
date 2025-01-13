217263f12338b13db5510fc4531a5c325b32de40
Agastya Krothapalli <agastyakro06@example.com>
2025-01-12 20:41:09 -0500

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,23 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  while(true){
-  std::cout << "$ ";
+    // Main 
+    while (true) {
+        std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  if (input == "exit 0"){
-    return false;
-  }
-  if (input.substr(0,5) == "echo"){
-    std::cout<< input.substr(5);
-    return;
-  }
-  std::cout<< input << ": command not found" << std::endl;
-  }
+        std::string input;
+        std::getline(std::cin, input);
+
+        if (input == "exit 0") {
+            return 0; // Exits the program
+        }
+
+        if (input.substr(0, 4) == "echo" && input.size() > 5) {
+            std::cout << input.substr(5) << std::endl;
+        } else {
+            std::cout << input << ": command not found" << std::endl;
+        }
+    }
+
+    return 0;
 }
